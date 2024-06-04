e42bc838dc930f89f1030b8aa8fdbc568677138f
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-06-04 07:43:24 +0300

    Updated the documentation.

--- NamedEntityRecognition/Slot.cs
+++ NamedEntityRecognition/Slot.cs
@@ -4,13 +4,26 @@ namespace NamedEntityRecognition
     {
         private SlotType type;
         private string tag;
-
+        
+        /// <summary>
+        /// Constructor for the Slot object. Slot object stores the information about more specific entities. The slot
+        /// type represents the beginning, inside or outside the slot, whereas tag represents the entity tag of the
+        /// slot.
+        /// </summary>
+        /// <param name="type">Type of the slot. B, I or O for beginning, inside, outside the slot respectively.</param>
+        /// <param name="tag">Tag of the slot.</param>
         public Slot(SlotType type, string tag)
         {
             this.type = type;
             this.tag = tag;
         }
 
+        /// <summary>
+        /// Second constructor of the slot for a given slot string. A Slot string consists of slot type and slot tag
+        /// separated with '-'. For example B-Person represents the beginning of a person. For outside tagging simple 'O' is
+        /// used.
+        /// </summary>
+        /// <param name="slot">Input slot string.</param>
         public Slot(string slot)
         {
             if (slot == "O" || slot == "o")
@@ -36,16 +49,28 @@ namespace NamedEntityRecognition
             }
         }
 
+        /// <summary>
+        /// Accessor for the type of the slot.
+        /// </summary>
+        /// <returns>Type of the slot.</returns>
         public SlotType GetType()
         {
             return type;
         }
 
+        /// <summary>
+        /// Accessor for the tag of the slot.
+        /// </summary>
+        /// <returns>Tag of the slot.</returns>
         public string GetTag()
         {
             return tag;
         }
 
+        /// <summary>
+        /// ToString method of the slot.
+        /// </summary>
+        /// <returns>Type and tag separated with '-'. If the type is outside, it returns 'O'.</returns>
         public override string ToString()
         {
             switch (type){
